da4958e7ef41d91c64ecd7a87efa6a4072067702
Rajeev singh <33415077+Rajeev0651@users.noreply.github.com>
2022-12-09 16:47:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'consumer_key' => '0Exmx0fSI37r4ZsdJxN0uBrtF',
-	'consumer_secret' => 'nQEkiT4KOpDokGd9v7KNDv99FbSx2HL3cfBmKGeZuOpvj77IPn',
+	'consumer_key' => '',
+	'consumer_secret' => '',
 	'url_login' => 'http://localhost/index.php',
 	'url_callback' =>'http://localhost/tweet_message.php',
 	];
